d746586487619af67e54e107a6404b79984c0436
Matheus Melo Antiquera <matheusantiquera@finchsolucoes.com.br>
2022-05-05 15:44:57 -0300

    test: :test_tube: adicionar os testes falhando - TDD
    Adicionar os testes de integração do CRUD de lojista com falha. Tentando aplicar um pouco do TDD
    
    ref: #3

---
+++ api/tests/Feature/StoreKeeperTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Faker\Generator;
+use Faker\Factory as Faker;
+use Tests\Traits\UserTestTrait;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class StoreKeeperTest extends TestCase
+{
+    use UserTestTrait;
+    use RefreshDatabase;
+
+    const API_STOREKEEPER_ROUTE = '/api/storekeeper/';
+    private Generator $fakerBr;
+
+    protected function setUp(): void
+    {
+        $this->fakerBr = Faker::create('pt_BR');
+        parent::setUp();
+    }
+
+    /**
+     * Deverá criar um lojista com sucesso
+     * @test
+     * @return void
+     */
+    public function shouldCreateAnUserSucessfully()
+    {
+        $response = $this->postJson(self::API_STOREKEEPER_ROUTE, $this->returnStoreKeeperInsertable($this->fakerBr)->toArray());
+
+        $response->assertStatus(201);
+        $this->assertTrue($response['success']);
+        $this->assertNotEmpty($response['data']);
+        $this->assertIsArray($response['data']);
+        $this->assertNotContains('balance', $response['data']);
+    }
+
+    /**
+     * Deverá mostrar os dados um lojista com sucesso
+     * @test
+     * @return void
+     */
+    public function shouldShowAnUser()
+    {
+        $storeKeeperCreated = $this->createStoreKeeperSuccessfully();
+        $response = $this->getJson(self::API_STOREKEEPER_ROUTE . $storeKeeperCreated->id);
+
+        $response->assertStatus(200);
+        $this->assertTrue($response['success']);
+        $this->assertNotEmpty($response['data']);
+        $this->assertIsArray($response['data']);
+        $this->assertNotContains('balance', $response['data']);
+    }
+
+    /**
+     * Deverá atualizar os dados um lojista com sucesso
+     * @test
+     * @return void
+     */
+    public function shouldUpdateAnUserSuccessfully()
+    {
+        $storeKeeperCreated = $this->createStoreKeeperSuccessfully();
+        $newLastName = $this->fakerBr->lastName();
+        $newEmail = $this->fakerBr->email();
+        $response = $this->putJson(self::API_STOREKEEPER_ROUTE . $storeKeeperCreated->id, [
+            "last_name" => $newLastName,
+            "email" => $newEmail
+        ]);
+        $response->assertStatus(200);
+        $this->assertTrue($response['success']);
+        $this->assertNotEmpty($response['data']);
+        $this->assertIsArray($response['data']);
+        $this->assertNotContains('balance', $response['data']);
+        $this->assertEquals($newLastName, $response['data']['last_name']);
+        $this->assertEquals($newEmail, $response['data']['email']);
+    }
+
+    /**
+     * Deverá deletar os dados um lojista com sucesso
+     * @test
+     * @return void
+     */
+    public function shouldDeleteAnUserSucessfully()
+    {
+        $storeKeeperCreated = $this->createStoreKeeperSuccessfully();
+        $response = $this->deleteJson(self::API_STOREKEEPER_ROUTE . $storeKeeperCreated->id);
+        $response->assertStatus(200);
+        $response->assertExactJson(['success' => true, "message" => "The storekeeper was deleted successfully"]);
+    }
+}
